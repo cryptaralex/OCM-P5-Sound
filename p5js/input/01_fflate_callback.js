9759b10715f0a7caa8676979f8cd921744baaef3
cryptaralex <34349928+cryptaralex@users.noreply.github.com>
2023-10-21 14:12:33 -0700

    Update 01_fflate_callback.js add sound

--- p5js/input/01_fflate_callback.js
+++ p5js/input/01_fflate_callback.js
@@ -4,4 +4,8 @@ function fflateCallback2() {
   const newScript = document.createElement('script');
   newScript.innerHTML = [p5Code].join(";\n");
   document.body.appendChild(newScript);
+  const p5Sound = fflate.strFromU8(fflate.gunzipSync(new Uint8Array(Array.from(atob(d4)).map((char) => char.charCodeAt(0)))));
+  const anotherScript = document.createElement('script');
+  anotherScript.innerHTML = [p5Sound].join(";\n");
+  document.body.appendChild(anotherScript);
 }
